91c6e66b9ba0744d4d33189a85f6ab06ff055cda
ThijsBertram <thijsbertram@gmail.com>
2025-01-03 14:17:58 +0100

    chore: delete old platforms folder

--- beetsplug/platforms/__init__.py
+++
@@ -1,4 +0,0 @@
-from beetsplug.platforms import *
-from beetsplug.platforms.platform_manager import PlatformManager
-from beetsplug.platforms.spotify import spotify_plugin
-# from beetsplug.platforms.youtube import youtube_plugin
